4cfaea19ded593e7809b81c353c35a45524ae7a8
= <=>
2024-01-20 17:32:30 +0530

    update CareerLayoutCard

--- src/components/CareerLayoutCard.tsx
+++ src/components/CareerLayoutCard.tsx
@@ -16,32 +16,22 @@ const CareerLayoutCard: React.FC<ICareerLayoutCardProps> = ({
 
   return (
     <article className='shadow-lg lg:shadow-none flex flex-col gap-6 lg:flex-row last:justify-between w-full p-12 last:w-full'>
-      <div className=''>
-        {image1 && (
-          <img
-            src={image1}
-            alt={title}
-            className={`w-full object-cover ${
-              isDotted1 ? 'hidden lg:block' : 'block lg:hidden'
-            }`}
-          />
-        )}
-      </div>
+      {image1 && (
+        <div className={`${isDotted1 ? 'hidden lg:block' : 'block'}`}>
+          <img src={image1} alt={title} className={`w-full object-cover`} />
+        </div>
+      )}
+
       <div>
         <h3 className='font-bold text-2xl mb-4 uppercase'>{title}</h3>
         <p className='text-lg tracking-widest max-w-[400px]'>{description}</p>
       </div>
-      <div>
-        {image2 && (
-          <img
-            src={image2}
-            alt={title}
-            className={`w-full object-cover ${
-              isDotted2 ? 'hidden lg:block' : 'block lg:hidden'
-            }`}
-          />
-        )}
-      </div>
+
+      {image2 && (
+        <div className={`${isDotted2 ? 'hidden lg:block' : 'block'}`}>
+          <img src={image2} alt={title} className={`w-full object-cover `} />
+        </div>
+      )}
     </article>
   );
 };
